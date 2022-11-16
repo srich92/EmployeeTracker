f47207629417742df9b0fb8119cf16e480d0dd85
srich92 <srichards646@gmail.com>
2022-11-15 20:55:22 -0500

    databse info complete

--- schema.sql
+++ schema.sql
@@ -15,3 +15,51 @@ CREATE TABLE role (
     INDEX dep_ind (department_id),
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
+CREATE TABLE employee (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER, 
+    INDEX role_ind (role_id),
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    manager_id INTEGER,
+    INDEX manager_ind (manager_id),
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
+
+INSERT INTO department (name)
+VALUES 
+('IT'),
+('Finance & Accounting'),
+('Sales & Marketing'),
+('Operations');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Sales Lead', 90000, 3),
+('Project Manager', 100000, 4),
+('Operations Manager', 90000, 4);
+('Developer', 80000, 1),
+('Engineer', 120000, 1),
+('Accountant', 10000, 2), 
+('Finanical Analyst', 150000, 2),
+('Marketing Coordindator', 70000, 3), 
+
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES 
+('John', 'Smith', 2, null),
+('Jordan', 'Smith', 1, 1),
+('Brittney', 'Smith', 4, null),
+('Melanie', 'Smith', 3, 3),
+('Hunter', 'Smith', 6, null),
+('Will', 'Smith', 5, 5),
+('Ian', 'Smith', 7, null),
+('Devyn', 'Smith', 8, 7);
+
+
+
+
+
+
